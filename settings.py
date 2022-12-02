bed43cf2ec003ea944012b120668569b8fbb450b
Alex_Melnikov <alexmelnikoff95@yandex.ru>
2022-12-02 20:59:07 +0300

    task_2 Создание функционала.

--- settings.py
+++ settings.py
@@ -4,3 +4,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 token = os.getenv('TOKEN')
+
+vk = os.getenv('VK')
+
+youtube = os.getenv('YOUTUBE')
--- telegram_api.py
+++ telegram_api.py
@@ -1,7 +1,7 @@
 import telebot
 from telebot import types
 
-from settings import token
+from settings import token, vk, youtube
 
 
 def telegram_bot(token):
@@ -9,19 +9,32 @@ def telegram_bot(token):
 
     @bot.message_handler(commands=['start'])
     def start_message(message):
-        bot.send_message(message.chat.id, 'Hellow')
+        bot.send_message(message.chat.id, 'Привет')
 
     @bot.message_handler(commands=['button'])
     def button_message(message):
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        item1 = types.KeyboardButton('Кнопка')
-        markup.add(item1)
+        markup = types.InlineKeyboardMarkup(row_width=2)
+        item1 = types.InlineKeyboardButton('Скачать видео с вконтакте', callback_data='vk')
+        item2 = types.InlineKeyboardButton('Скачать видео с YouTube', callback_data='yotube')
+
+        markup.add(item1, item2)
+
         bot.send_message(message.chat.id, 'Выберите что вам надо', reply_markup=markup)
+
+    @bot.callback_query_handler(func=lambda call: True)
+    def callback(call):
+        if call.message:
+            if call.data == 'vk':
+                bot.send_message(call.message.chat.id, vk)
+            elif call.data == 'yotube':
+                bot.send_message(call.message.chat.id, youtube)
 
     @bot.message_handler(content_types='text')
     def message_return(message):
-        if message.text.lower() == 'кнопка':
-            bot.send_message(message.chat.id, 'https://www.youtube.com/watch?v=x-VB3b4pKcU&t=44s')
+        if message.text.lower() == 'вк':
+            bot.send_message(message.chat.id, vk)
+        if message.text.lower() == 'ютуб':
+            bot.send_message(message.chat.id, youtube)
 
     bot.infinity_polling()
 
